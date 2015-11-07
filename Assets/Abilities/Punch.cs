19077a9931f3cbc2f593990be1cac89ba2aac0c8
Michl Steglich <mrgreaka@gmail.com>
2015-11-07 17:06:12 +0100

    Punch

---
+++ Assets/Abilities/Punch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Punch : AttackScript
+{
+
+    public override void UseAttack(Transform attackingPlayerTransform)
+    {
+        //Todo epic shit from here..
+        Debug.Log("Punch");
+
+        Vector3 playerPos = attackingPlayerTransform.position;
+        Vector3 playerDir = attackingPlayerTransform.forward;
+
+        var opfer = Physics2D.Raycast(playerPos + playerDir, playerDir, 2);
+        opfer.rigidbody.AddForce(playerDir * 500);
+    }
+}
