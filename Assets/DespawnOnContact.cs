3c4d57c213cf0462c9a55237ecfedef2b20df61b
Gnaarf <gnaarf@gmx.de>
2015-11-07 18:17:59 +0100

    fixed despawn and explosion of fireball

---
+++ Assets/DespawnOnContact.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DespawnOnContact : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Fireball.cs
+++ Assets/Fireball.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Fireball : AttackScript
 {
     public Rigidbody2D FireballPrefab;
+    public float SpawnDistance = 2.0F;
 
     public override void UseAttack(Transform attackingPlayerTransform)
     {
@@ -13,7 +14,7 @@ public class Fireball : AttackScript
         Vector3 playerPos = attackingPlayerTransform.position;
         Vector3 playerDir = attackingPlayerTransform.forward;
 
-        Rigidbody2D fireBall = (Rigidbody2D)Instantiate(FireballPrefab, playerPos + playerDir * 1.3F, Quaternion.identity);
+        Rigidbody2D fireBall = (Rigidbody2D)Instantiate(FireballPrefab, playerPos + playerDir * SpawnDistance, Quaternion.identity);
         fireBall.AddForce(playerDir * 500);
 
     }
