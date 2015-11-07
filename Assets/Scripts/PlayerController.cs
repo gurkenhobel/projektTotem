03a8c9344fdd234b92bf043e5030b7c622ea6973
Michl Steglich <mrgreaka@gmail.com>
2015-11-07 05:01:00 +0100

    Abilities und MainScene aktualisiert

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,8 @@ public class PlayerController : MonoBehaviour {
 
     public string InputKey;
 
-    public float HorizontalSpeed = 2.0f;
-    public float VerticalSpeed = 2.0f;
+    public float HorizontalSpeed = 20.0f;
+    public float VerticalSpeed = 400.0f;
     private int jumpCount;
 
 	// Use this for initialization
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float h = HorizontalSpeed * Input.GetAxis("Horizontal_" + InputKey);
-        
+
         Vector2 movement = new Vector2(h, 0f);
 
         if (Input.GetButtonDown("Jump_" + InputKey) && jumpCount < 2)
@@ -34,5 +34,13 @@ public class PlayerController : MonoBehaviour {
         {
             jumpCount = 0;   
         }
+
+        RotatePlayer();
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetAxis("Horizontal_" + InputKey) != 0)
+            transform.rotation = Quaternion.Euler(0, 90 * Mathf.Sign(Input.GetAxis("Horizontal_" + InputKey)), 0);
     }
 }
