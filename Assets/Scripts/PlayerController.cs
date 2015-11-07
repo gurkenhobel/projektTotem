d9bc428403844d7f83b52ce68fdb04dcb22cb59a
Till Höppner <hoeppner.till+gh@gmail.com>
2015-11-07 14:01:33 +0100

    Move to FixedUpdate, clean up code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,25 +18,20 @@ public class PlayerController : MonoBehaviour {
     public float MaxHorizontalSpeed = 20;
     private int jumpCount;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
         aus = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	    bool isDrüber = Mathf.Abs(rb.velocity.x) < MaxHorizontalSpeed;
-        var h = isDrüber ? HorizontalSpeed*Input.GetAxis("Horizontal_" + InputKey) : 0;
+    }
 
-	    Vector2 movement = new Vector2(-h, 0f);
+    void FixedUpdate() {
+        bool tooFast = Mathf.Abs(rb.velocity.x) > MaxHorizontalSpeed;
+        var input = Input.GetAxis("Horizontal_" + InputKey);
+        var h = tooFast ? MaxHorizontalSpeed - (HorizontalSpeed * input) : HorizontalSpeed * input;
 
-        animator.SetBool("Jumping", true);
-        animator.SetBool("Walking", h == 0 ? false : true);
+        Vector2 movement = new Vector2(-h, 0f);
 
-        if (Input.GetButtonDown("Jump_" + InputKey) && jumpCount < 1)
-        {
+        if (Input.GetButtonDown("Jump_" + InputKey) && jumpCount == 0) {
             aus.clip = Jump;
             aus.Play();
 
@@ -44,16 +39,17 @@ public class PlayerController : MonoBehaviour {
             jumpCount++;
         }
 
-        Debug.Log("isDrüber: " + isDrüber);
+        Debug.Log("tooFast: " + tooFast);
         Debug.Log("MaxHorizontalSpeed: " + MaxHorizontalSpeed);
         Debug.Log("h: " + h);
         Debug.Log("Speed: " + Mathf.Abs(rb.velocity.x));
-	    rb.AddForce(new Vector2(Mathf.Sign(movement.x) != Mathf.Sign(rb.velocity.x) ? movement.x* 3 : movement.x, movement.y));    //wenn man die kommentare raus nimmt, gilt extra speed nur fürs bremsen
+        rb.AddForce(new Vector2(Mathf.Sign(movement.x) != Mathf.Sign(rb.velocity.x) ? movement.x * 3 : movement.x, movement.y));
 
-	    if(rb.velocity.y == 0)
-        {
-            if(jumpCount > 0)
-            {
+        animator.SetBool("Jumping", true);
+        animator.SetBool("Walking", h != 0);
+
+        if (rb.velocity.y == 0) {
+            if (jumpCount > 0) {
                 aus.clip = Land;
                 aus.Play();
             }
@@ -61,11 +57,14 @@ public class PlayerController : MonoBehaviour {
 
             animator.SetBool("Jumping", false);
         }
+    }
+
+    // Update is called once per frame
+    void Update() {
         RotatePlayer();
     }
 
-    void RotatePlayer()
-    {
+    void RotatePlayer() {
         if (Input.GetAxis("Horizontal_" + InputKey) != 0)
             transform.rotation = Quaternion.Euler(0, -90 * Mathf.Sign(Input.GetAxis("Horizontal_" + InputKey)), 0);
     }
