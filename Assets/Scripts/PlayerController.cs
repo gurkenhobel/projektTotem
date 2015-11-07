6da6d20f2456e18f54ecff7743da7b12993e096f
Daniel Mielnicki <daniel.mielnicki@gmx.de>
2015-11-07 02:58:44 +0100

    Made a PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    public float HorizontalSpeed = 2.0f;
+    public float VerticalSpeed = 2.0f;
+    private int jumpCount;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = HorizontalSpeed * Input.GetAxis("Horizontal");
+        
+        Vector2 movement = new Vector2(h, 0f);
+
+        if (Input.GetButtonDown("Jump") && jumpCount < 2)
+        {
+            movement += Vector2.up * VerticalSpeed;
+            jumpCount++;
+        }
+
+        rb.AddForce(movement);
+
+        if(rb.velocity.y == 0)
+        {
+            jumpCount = 0;   
+        }
+    }
+}
