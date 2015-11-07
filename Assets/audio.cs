6c99af62e8497e0f6e22339fe2402fb9dee0e991
Maximilian Deubel <maximilian.deubel@gmail.com>
2015-11-07 03:49:39 +0100

    Sound testing 101

---
+++ Assets/audio.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class audio : MonoBehaviour {
+    public AudioSource aus;
+    float timeStopped = 0;
+	// Use this for initialization
+	void Start () {
+        aus = GetComponent<AudioSource>();
+        aus.Play();
+        aus.volume = 0;
+        aus.time = 0;
+        timeStopped = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (aus.time > aus.clip.length - 2F)
+            aus.volume -= 0.5F * Time.deltaTime;
+        else
+            if ((timeStopped == 0) && (aus.volume < 2F)) aus.volume += 0.5F * Time.deltaTime;
+        if (!aus.isPlaying) {
+            if (timeStopped == 0) timeStopped = Time.time;
+           if (Time.time - timeStopped >= 1F) {
+                aus.Play();
+                timeStopped = 0;
+            }
+        }
+    }
+}
