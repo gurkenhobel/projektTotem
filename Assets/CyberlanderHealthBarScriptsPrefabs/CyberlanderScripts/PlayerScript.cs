02bf3a9651988f3db0e9d24c76b5ffdce4577223
Cyberlander <felix.froehlich1@gmx.net>
2015-11-07 03:39:50 +0100

    Health

---
+++ Assets/CyberlanderHealthBarScriptsPrefabs/CyberlanderScripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		handleMovement ();
+	
+	}
+
+	private void handleMovement()
+	{
+		float translation = speed * Time.deltaTime;
+		transform.Translate (new Vector3 (Input.GetAxis ("Horizontal")*translation, 0, Input.GetAxis ("Vertical")*translation));
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.name == "Health") 
+		{
+			Debug.Log ("Getting Healed");
+		} else if (other.name == "Damage") 
+		{
+			Debug.Log("Getting Damaged");
+		}
+
+	}
+}
